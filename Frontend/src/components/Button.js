ba51e2160ade5d64df9fe0dfef41796c8a5230b2
17sarab02 <96364469+17sarab02@users.noreply.github.com>
2023-03-31 09:40:13 +0530

    Delete Frontend directory

--- Frontend/src/components/Button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../componentCss/Button.css'
-
-export default function Button(props){
-    return (
-        <button onClick={props.onClick}>{props.children}</button>
-    )
-}
